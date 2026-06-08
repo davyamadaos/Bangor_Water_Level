103b01aa0eda1d76f3bf0d92f90fdcce10467c9f
davyamadaos <dfmapping@gmail.com>
2026-06-08 22:23:23 +0100

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -1,4 +1,7 @@
 import io, json, zipfile, requests, pandas as pd
+import os
+
+os.makedirs("data", exist_ok=True)
 URL='https://epawebapp.epa.ie/Hydronet/output/internet/stations/CAS/33008/Q/3_months.zip'
 z=zipfile.ZipFile(io.BytesIO(requests.get(URL,timeout=60).content))
 csv=[n for n in z.namelist() if n.endswith('.csv')][0]
